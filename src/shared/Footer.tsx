a698500916b04c189336d6ea4692981a24500e34
Victor <victorolaolubanjo@yahoo.com>
2024-02-18 11:55:25 -0500

    theme added

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -1,10 +1,19 @@
+import { motion } from "framer-motion";
+
 const Footer = () => {
   return (
-    <footer className="mt-20 bg-[#0d141c] text-white p-10">
+    <motion.footer
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      className="mt-20 text-white p-10"
+    >
       <div className="flex items-center text-center justify-center h-[50px]">
-        <p className="text-sm">©Copyright 2024 Antisocial_Dev. All rights reserved</p>
+        <p className="text-sm">
+          ©Copyright 2024 Antisocial_Dev. All rights reserved
+        </p>
       </div>
-    </footer>
+    </motion.footer>
   );
 };
 
--- src/shared/Navbar.tsx
+++ src/shared/Navbar.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { toggleSidebar } from "@/reducers/sidebar-slice";
+import { toggleTheme } from "@/reducers/theme.reducer";
 import { RootState } from "@/store/app-store";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { Root } from "postcss";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -13,15 +15,24 @@ const NavBar = () => {
     (state: RootState) => state.sidebar.isOpen
   );
   const dispatch = useDispatch();
+  const router = useRouter();
 
   const menuClick = async () => {
     await dispatch(toggleSidebar());
-   
   };
+
+  const setTheme = ()=> {
+    let theme = localStorage.getItem('theme');
+    dispatch(toggleTheme(theme=== 'light'? 'dark': 'light'));
+  }
 
   const scrolltoHash =  (element_id: string) => {
     const element = document.getElementById(element_id)
     window.scrollTo({left: 0, top: element!.offsetTop - document.getElementsByClassName('navbar')[0].clientHeight, behavior: 'smooth'});
+  }
+
+  const reloadPage = ()=>{
+    router.refresh();
   }
 
 
@@ -34,7 +45,7 @@ const NavBar = () => {
       transition={{ duration: 1 }}
       className="flex justify-between items-center gap-8 p-10 navbar"
     >
-      <div className="flex items-center gap-5">
+      <div onClick={reloadPage} className="flex items-center gap-5">
         <Image
           className="w-[30px] sm:w-[30px]"
           src="/icons/home.svg"
@@ -69,7 +80,7 @@ const NavBar = () => {
         <p className="cursor-pointer" onClick={()=>scrolltoHash('project-section')}>Projects</p>
         <p className="cursor-pointer" onClick={()=>scrolltoHash('skills-section')}>Skills</p>
         <p className="cursor-pointer" onClick={()=>scrolltoHash('contact-section')}>Contact</p>
-        <div className="flex cursor-pointer items-center justify-center w-[45px] h-[45px] p-3 bg-[#E8EDF2] rounded-[12px]">
+        <div onClick={setTheme} className="flex cursor-pointer items-center justify-center w-[45px] h-[45px] p-3 bg-[#E8EDF2] rounded-[12px]">
           <Image
             src="/icons/theme-icon.svg"
             alt="theme toogle"
